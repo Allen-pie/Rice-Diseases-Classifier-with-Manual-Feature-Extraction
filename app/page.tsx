e37c2548d5efeb8088f5fbc3f93463858b778d42
Pieter Allen <pieter.allen@binus.ac.id>
2025-12-03 16:58:03 +0700

    #feat frontend template

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,38 @@
-import Image from "next/image";
+import { BentoGrid6 } from "@/components/custom/bento-grid-6";
+import { FaqSection2 } from "@/components/custom/faq-section-2";
+import { FeatureSection9 } from "@/components/custom/feature-section-9";
+import { Footer1 } from "@/components/custom/footer-1";
+import { HeroSection2 } from "@/components/custom/hero-section-2";
+import { LogoSection10 } from "@/components/custom/logo-section-7";
+import { LpNavbar1 } from "@/components/custom/lp-navbar-1";
+import { PricingSection3 } from "@/components/custom/pricing-section-3";
+import { StatsSection4 } from "@/components/custom/stats-section-4";
+import TestimonialsSection1 from "@/components/custom/testimonials-section-1";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main>
+      <LpNavbar1 />
+      <HeroSection2 />
+      <LogoSection10 />
+      <TestimonialsSection1
+        quote="MindSpace is like having a photographic memory for every meeting. We reduced the follow-up emails by 80%."
+        authorName="David Park"
+        authorRole="Engineering Manager at TechCorp"
+        avatarSrc="/DavidPark.png"
+      />
+      <BentoGrid6 />
+      <FeatureSection9 />
+      <StatsSection4 />
+      <TestimonialsSection1
+        quote="At TechStar, 43% of meeting content was forgotten within 24 hours. Now, the AI summaries are better than my own notes!"
+        authorName="Monica Kurt"
+        authorRole="Project Manager at TechStar"
+        avatarSrc="/MonicaKurt.png"
+      />
+      <PricingSection3 />
+      <FaqSection2 />
+      <Footer1 />
+    </main>
   );
 }
